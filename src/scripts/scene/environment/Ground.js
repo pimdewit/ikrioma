6c041e7865048403db3e0e5c3886d297476efa7b
Pim de Wit <pim@pdw.io>
2018-03-21 22:06:05 +0000

    feat(ground): nicer looking floor

--- src/scripts/scene/environment/Ground.js
+++ src/scripts/scene/environment/Ground.js
@@ -4,13 +4,13 @@ class Ground extends Object3D {
   constructor() {
     super();
 
-    const geometry = new PlaneBufferGeometry(10000, 10000);
-    const material = new MeshPhongMaterial({ color: 0xffbf00, specular: 0x050505 });
+    const geometry = new PlaneBufferGeometry(32, 32);
+    const material = new MeshPhongMaterial({ color: 0x788196, specular: 0x111111 });
 
     const mesh = new Mesh(geometry, material);
     mesh.rotation.x = -Math.PI / 2;
-    mesh.position.y = -10;
     mesh.receiveShadow = true;
+
     this.add(mesh);
   }
 }
