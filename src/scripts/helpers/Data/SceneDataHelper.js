f1b73793c77a95a13668b57ce8808c9c2d4325e0
Pim de Wit <pim@pdw.io>
2018-04-06 12:20:31 +0100

    chore(SceneDataHelper): added documentation

--- src/scripts/helpers/Data/SceneDataHelper.js
+++ src/scripts/helpers/Data/SceneDataHelper.js
@@ -3,18 +3,57 @@ import DataHelper from './DataHelper';
 /**
  * Take the Scene renderer and extract info from it.
  */
+
+/**
+ * @author Pim de Wit / https://pdw.io
+ *
+ * @class
+ * @classdesc
+ * Take the Scene renderer and extract info from it, then display it in HTML elements.
+ *
+ * @since 0.0.1
+ * @extends {DataHelper}
+ */
 class SceneDataHelper extends DataHelper {
   constructor(renderer) {
     super();
+
+    /**
+     * The data source.
+     *
+     * @since 0.0.1
+     * @private
+     * @readonly
+     *
+     * @type {Renderer}
+     */
     this._renderer = renderer;
 
     this._createDOM(this.sceneData);
   }
 
+  /**
+   * A simple shorthand to the class' data object.
+   *
+   * @memberOf SceneDataHelper
+   * @since 0.0.1
+   * @public
+   * @readonly
+   *
+   * @type {object}
+   * @returns {object<string>}
+   */
   get sceneData() {
     return this._renderer.engine.info.render;
   }
 
+  /**
+   * Render the processed data to display into its own assigned DOM elements.
+   *
+   * @memberOf CameraDataHelper
+   * @since 0.0.1
+   * @public
+   */
   render() {
     if (!this._active) return;
 
