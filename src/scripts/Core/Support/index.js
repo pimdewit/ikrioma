1593450020fd32eb6c3f04d8263a8cc6b3106af4
Pim de Wit <pim@pdw.io>
2018-05-08 17:39:46 +0100

    feat(architecture): encapsulating diagnostics related methods in a namespace

---
+++ src/scripts/Core/Support/index.js
@@ -0,0 +1,21 @@
+import {isDebug} from './isDebug';
+import {isEditor} from './isEditor';
+import {parseBoolean} from './parseBoolean';
+import {isIE11} from './isIE11';
+import {isIframe} from './isIframe';
+import {isIOS} from './isIOS';
+import {isIOS9OrLess} from './isIOS9OrLess';
+import {isMobile} from './isMobile';
+import {isSafari} from './isSafari';
+
+
+export {
+  isDebug,
+  isEditor,
+  isIE11,
+  isIframe,
+  isIOS,
+  isIOS9OrLess,
+  isMobile,
+  isSafari
+}
