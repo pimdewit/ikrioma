2ee7beb557bf679e9e81c3026c81ef62b34c39a9
Pim de Wit <pim@pdw.io>
2018-05-08 17:19:51 +0100

    feat(architecture): encapsulating easing related methods in a namespace

---
+++ src/scripts/Core/Easing/index.js
@@ -0,0 +1,61 @@
+import {easeNone} from './easeNone';
+import {easeInQuad} from './easeInQuad';
+import {easeOutQuad} from './easeOutQuad';
+import {easeInOutQuad} from './easeInOutQuad';
+import {easeOutInQuad} from './easeOutInQuad';
+import {easeInCubic} from './easeInCubic';
+import {easeOutCubic} from './easeOutCubic';
+import {easeInOutCubic} from './easeInOutCubic';
+import {easeOutInCubic} from './easeOutInCubic';
+import {easeInQuart} from './easeInQuart';
+import {easeOutQuart} from './easeOutQuart';
+import {easeInOutQuart} from './easeInOutQuart';
+import {easeOutInQuart} from './easeOutInQuart';
+import {easeInQuint} from './easeInQuint';
+import {easeOutQuint} from './easeOutQuint';
+import {easeInOutQuint} from './easeInOutQuint';
+import {easeOutInQuint} from './easeOutInQuint';
+import {easeInSine} from './easeInSine';
+import {easeOutSine} from './easeOutSine';
+import {easeInOutSine} from './easeInOutSine';
+import {easeOutInSine} from './easeOutInSine';
+import {easeInExpo} from './easeInExpo';
+import {easeOutExpo} from './easeOutExpo';
+import {easeInOutExpo} from './easeInOutExpo';
+import {easeOutInExpo} from './easeOutInExpo';
+import {easeInCirc} from './easeInCirc';
+import {easeOutCirc} from './easeOutCirc';
+import {easeInOutCirc} from './easeInOutCirc';
+import {easeOutInCirc} from './easeOutInCirc';
+
+export {
+  easeNone,
+  easeInQuad,
+  easeOutQuad,
+  easeInOutQuad,
+  easeOutInQuad,
+  easeInCubic,
+  easeOutCubic,
+  easeInOutCubic,
+  easeOutInCubic,
+  easeInQuart,
+  easeOutQuart,
+  easeInOutQuart,
+  easeOutInQuart,
+  easeInQuint,
+  easeOutQuint,
+  easeInOutQuint,
+  easeOutInQuint,
+  easeInSine,
+  easeOutSine,
+  easeInOutSine,
+  easeOutInSine,
+  easeInExpo,
+  easeOutExpo,
+  easeInOutExpo,
+  easeOutInExpo,
+  easeInCirc,
+  easeOutCirc,
+  easeInOutCirc,
+  easeOutInCirc
+}
